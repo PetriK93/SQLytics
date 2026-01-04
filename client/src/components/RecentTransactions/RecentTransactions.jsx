4556184b476f59c3a1c45b80d9e9d9978ccbf65c
Petri Korpikoski <petrik.dev@outlook.com>
2026-01-04 09:02:44 +0200

    Finished the recent transactions section for now

--- client/src/components/RecentTransactions/RecentTransactions.jsx
+++ client/src/components/RecentTransactions/RecentTransactions.jsx
@@ -1,13 +1,40 @@
 import styles from "./RecentTransactionsStyles.module.css";
 import recentTransactionsIcon from "../../assets/recent_transactions_green.png";
+import TransactionCard from "../TransactionCard/TransactionCard.jsx";
 
 function RecentTransactions({ title }) {
+  const currentDate = new Date().toLocaleString("en-US");
+
   return (
     <div className={styles.container}>
       <div className={styles.horizontalWrapper}>
         <img src={recentTransactionsIcon} className={styles.transactionsIcon} />
         <p className={styles.title}>{title}</p>
       </div>
+      <TransactionCard
+        id="abc-123"
+        name="Alexandria Catherine Montgomery-Smith"
+        date={currentDate}
+        amount="694.20"
+      />
+      <TransactionCard
+        id="cba-321"
+        name="Maximilian Alexander Jonathan von Hohenberg III"
+        date={currentDate}
+        amount="10000.20"
+      />
+      <TransactionCard
+        id="jhg-321"
+        name="Rafael"
+        date={currentDate}
+        amount="10"
+      />
+      <TransactionCard
+        id="gha-296"
+        name="Petri Korpikoski"
+        date={currentDate}
+        amount="1300204504958725312"
+      />
     </div>
   );
 }
--- client/src/components/TransactionCard/TransactionCard.jsx
+++ client/src/components/TransactionCard/TransactionCard.jsx
@@ -3,10 +3,12 @@ import styles from "./TransactionCardStyles.module.css";
 function TransactionCard({ id, name, date, amount }) {
   return (
     <div className={styles.container}>
-      <p className={styles.id}>{id}</p>
-      <p className={styles.name}>{name}</p>
+      <div className={styles.verticalWrapper}>
+        <p className={styles.id}>{id}</p>
+        <p className={styles.name}>{name}</p>
+      </div>
       <p className={styles.date}>{date}</p>
-      <div className={styles.amountCard}>
+      <div className={styles.amountContainer}>
         <p className={styles.amount}>{amount}</p>
       </div>
     </div>
